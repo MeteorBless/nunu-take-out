90605af746d3a98314fd056ea8d151f1c4911031
Ryuzaki <2677017641@qq.com>
2024-06-01 11:15:43 +0800

    微信支付功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -55,4 +55,14 @@ public interface OrderMapper {
      */
     @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
     Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+
 }
